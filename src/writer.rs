e484ab54f62c421a3f538ab82dbcfb91139c91d6
Lars Francke <lars.francke@gmail.com>
2021-06-02 15:46:43 +0200

    Make it work across threads

--- src/writer.rs
+++ src/writer.rs
@@ -4,8 +4,8 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum PropertiesWriterError {
-    #[error("Error creating properties file")]
-    PropertiesError(#[from] java_properties::PropertiesError),
+    #[error("Error creating properties file: {0}")]
+    PropertiesError(String),
 
     #[error("Error converting properties file byte array to UTF-8")]
     FromUtf8Error(#[from] std::string::FromUtf8Error),
@@ -15,6 +15,7 @@ pub fn create_properties_file(
     properties: &HashMap<String, String>,
 ) -> Result<String, PropertiesWriterError> {
     let mut output = Vec::new();
-    write(&mut output, &properties)?;
+    write(&mut output, &properties)
+        .map_err(|err| PropertiesWriterError::PropertiesError(err.to_string()))?;
     Ok(String::from_utf8(output)?)
 }
