c784188a3d2213004cec7c2ef16672e6a8dd3eb6
maltesander <malte.sander.it@gmail.com>
2021-06-29 16:01:25 +0200

    Created generic java properties writer that works with HashMap and BTreeMap.

--- src/writer.rs
+++ src/writer.rs
@@ -1,5 +1,5 @@
-use java_properties::write;
-use std::collections::HashMap;
+use java_properties::{PropertiesError, PropertiesWriter};
+use std::io::Write;
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq)]
@@ -17,19 +17,44 @@ pub enum PropertiesWriterError {
 ///
 /// The behavior is based on https://docs.oracle.com/javase/7/docs/api/java/util/Properties.html
 /// and is adapted to "java.util.Properties" if ambiguous or incomplete.
-pub fn create_java_properties_file(
-    properties: &HashMap<String, String>,
-) -> Result<String, PropertiesWriterError> {
+pub fn create_java_properties<'a, T>(properties: T) -> Result<String, PropertiesWriterError>
+where
+    T: Iterator<Item = (&'a String, &'a Option<String>)>,
+{
     let mut output = Vec::new();
-    write(&mut output, &properties)
+    write_java_properties(&mut output, properties)
         .map_err(|err| PropertiesWriterError::PropertiesError(err.to_string()))?;
     Ok(String::from_utf8(output)?)
 }
 
+/// Generic method to write java properties
+/// Accepts HashMap<String, Option<String>> or BTreeMap<String, Option<String>>.
+pub fn write_java_properties<'a, W, T>(writer: W, properties: T) -> Result<(), PropertiesError>
+where
+    W: Write,
+    T: Iterator<Item = (&'a String, &'a Option<String>)>,
+{
+    let mut writer = PropertiesWriter::new(writer);
+    for (k, v) in properties {
+        if let Some(value) = v {
+            if value.is_empty() {
+                writer.write(&k, "\"\"")?;
+            } else {
+                writer.write(&k, &value)?;
+            }
+        } else {
+            writer.write(&k, "")?;
+        }
+    }
+
+    writer.flush()?;
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::writer::{create_java_properties_file, PropertiesWriterError};
-    use std::collections::HashMap;
+    use crate::writer::{create_java_properties, write_java_properties, PropertiesWriterError};
+    use std::collections::{BTreeMap, HashMap};
 
     const PROPERTY_1: &str = "property";
     const PROPERTY_2: &str = "property2";
@@ -43,42 +68,71 @@ mod tests {
     #[test]
     fn test_writer_ok() -> Result<(), PropertiesWriterError> {
         let mut map = HashMap::new();
-        map.insert(PROPERTY_1.to_string(), VALUE_OK.to_string());
-        map.insert(PROPERTY_2.to_string(), VALUE_OK_2.to_string());
+        map.insert(PROPERTY_1.to_string(), Some(VALUE_OK.to_string()));
+        map.insert(PROPERTY_2.to_string(), Some(VALUE_OK_2.to_string()));
 
-        let result = create_java_properties_file(&map)?;
+        let result = create_java_properties(map.iter())?;
 
-        map.insert(PROPERTY_2.to_string(), VALUE_OK_2_ESCAPED.to_string());
-        assert_eq!(result, calculate_result(&map));
+        map.insert(PROPERTY_2.to_string(), Some(VALUE_OK_2_ESCAPED.to_string()));
+        assert_eq!(result, calculate_result(map.iter()));
         Ok(())
     }
 
     #[test]
     fn test_writer_escape() -> Result<(), PropertiesWriterError> {
         let mut map = HashMap::new();
-        map.insert(PROPERTY_1.to_string(), VALUE_URL.to_string());
+        map.insert(PROPERTY_1.to_string(), Some(VALUE_URL.to_string()));
 
-        let result = create_java_properties_file(&map)?;
+        let result = create_java_properties(map.iter())?;
 
-        map.insert(PROPERTY_1.to_string(), VALUE_URL_ESCAPED.to_string());
-        assert_eq!(result, calculate_result(&map));
+        map.insert(PROPERTY_1.to_string(), Some(VALUE_URL_ESCAPED.to_string()));
+        assert_eq!(result, calculate_result(map.iter()));
         Ok(())
     }
 
     #[test]
     fn test_writer_no_utf8() {
         let mut map = HashMap::new();
-        map.insert(PROPERTY_1.to_string(), UTF8_ERROR.to_string());
+        map.insert(PROPERTY_1.to_string(), Some(UTF8_ERROR.to_string()));
 
-        let result = create_java_properties_file(&map);
+        let result = create_java_properties(map.iter());
         assert!(result.is_err());
     }
 
-    fn calculate_result(properties: &HashMap<String, String>) -> String {
+    #[test]
+    fn test_write_java_properties() {
+        let mut btree_map = BTreeMap::new();
+        btree_map.insert("normal".to_string(), Some("normal".to_string()));
+        btree_map.insert("empty".to_string(), Some("".to_string()));
+        btree_map.insert("none".to_string(), None);
+
+        let expected = "empty=\"\"\nnone=\nnormal=normal\n";
+
+        let mut output = Vec::new();
+        write_java_properties(&mut output, btree_map.iter())
+            .map_err(|err| PropertiesWriterError::PropertiesError(err.to_string()))
+            .unwrap();
+
+        let result = String::from_utf8(output).unwrap();
+        assert_eq!(result, expected);
+    }
+
+    fn calculate_result<'a, T>(properties: T) -> String
+    where
+        T: Iterator<Item = (&'a String, &'a Option<String>)>,
+    {
         let mut result = String::new();
 
-        for (key, value) in properties {
-            result.push_str(&format!("{}={}\n", key, value));
+        for (k, v) in properties {
+            if let Some(value) = v {
+                if value.is_empty() {
+                    result.push_str(&format!("{}={}\n", &k, "\"\""));
+                } else {
+                    result.push_str(&format!("{}={}\n", &k, &value));
+                }
+            } else {
+                result.push_str(&format!("{}={}\n", &k, ""));
+            }
         }
 
         result
